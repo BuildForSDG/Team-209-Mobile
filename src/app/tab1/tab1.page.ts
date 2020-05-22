3e445598b11ec6054223b41046525194b1f096a9
Philip Boye-Doku <philip.boye-doku@ashesi.local>
2020-05-22 18:29:47 +0000

    Added geolocation

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Plugins, Capacitor, CameraResultType } from '@capacitor/core';
 
 @Component({
@@ -6,16 +6,22 @@ import { Plugins, Capacitor, CameraResultType } from '@capacitor/core';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
   constructor() {}
+
+  ngOnInit() {
+    // this.pickLocation();
+  }
 
   image: File;
   description: string;
+  lat: number;
+  lng: number;
   report = {
     user_id: '',
     location: {
-      lat: '',
-      lng: ''
+      lat: this.lat,
+      lng: this.lng
     },
     description: '',
     image_url: '',
@@ -23,29 +29,43 @@ export class Tab1Page {
     date: new Date()
   };
 
-  async pickImage() {
-    const image = await Plugins.Camera.getPhoto({
+  pickImage() {
+    const image = Plugins.Camera.getPhoto({
       quality: 60,
       allowEditing: true,
       resultType: CameraResultType.Base64
-    });
+    })
+      .then((image) => {
+        var imageUrl = image.base64String;
+        console.log('image-string', image);
+        // Can be set to the src of an image now
+        this.report.image_url = 'data:image/jpeg;base64,' + imageUrl;
+        let imageFile;
+        try {
+          imageFile = this.base64toBlob(imageUrl.replace('data:image/jpeg;base64,', ''), 'image/jpeg');
+          this.report.image = imageFile;
+          console.log('image', this.image);
 
-    var imageUrl = image.base64String;
-    console.log('image-string', image);
-    // Can be set to the src of an image now
-    this.report.image_url = 'data:image/jpeg;base64,' + imageUrl;
-    //console.log('src', this.report.image_url);
-    let imageFile;
-    try {
-      imageFile = this.base64toBlob(imageUrl.replace('data:image/jpeg;base64,', ''), 'image/jpeg');
-      this.report.image = imageFile;
-      console.log('image', this.image);
-    } catch (error) {
-      console.log('conversion error', error);
-      return;
-    }
+          this.pickLocation();
+        } catch (error) {
+          console.log('Base64 conversion error:', error);
+          return;
+        }
+      })
+      .catch((error) => {
+        console.log('camera error:', error);
+      });
+
     this.report.description = this.description;
     console.log('imageFile', this.report);
+  }
+
+  async pickLocation() {
+    const coordinates = await Plugins.Geolocation.getCurrentPosition();
+    console.log('Current', coordinates);
+    this.report.location.lat = coordinates.coords.latitude;
+    this.report.location.lng = coordinates.coords.longitude;
+    console.log(this.report);
   }
 
   base64toBlob(base64Data, contentType) {
@@ -68,4 +88,6 @@ export class Tab1Page {
     }
     return new Blob(byteArrays, { type: contentType });
   }
+
+  submit() {}
 }
